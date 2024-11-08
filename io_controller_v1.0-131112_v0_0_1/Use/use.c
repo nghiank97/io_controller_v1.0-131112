08f46b28a7cb5f5bc1ce1fcb435098ee4d59fa7e
nghiank97 <nghia.nk97@gmail.com>
2024-11-08 22:36:20 +0700

    user

---
+++ io_controller_v1.0-131112_v0_0_1/Use/use.c
@@ -0,0 +1,13 @@
+
+#include "gpio.h"
+#include "time.h"
+#include "use.h"
+
+void setup(){
+    gpio_init();
+    timer_init();
+}
+
+void loop(){
+
+}
---
+++ io_controller_v1.0-131112_v0_0_1/Use/use.h
@@ -0,0 +1,13 @@
+
+
+#ifndef _USE_H
+#define _USE_H
+
+#include "binary.h"
+#include "utils.h"
+#include "main.h"
+
+extern void setup(void);
+extern void loop(void);
+
+#endif /* _USE_H */
